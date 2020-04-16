8358f9a3d80a118a297daedc392c2bf81e30f508
fbc623 <fbc623gmail.com>
2020-04-16 16:44:15 -0300

    frist commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '42af784730eb6302bc814ce75acbd4c4',
+    expiresIn: '1d',
+  },
+};
---
+++ src/database/migrations/1587042371699-CreateRelationAppointmentUsers.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+
+export default class CreateRelationAppointmentUsers1587042371699
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'appointments',
+      new TableForeignKey({
+        name: 'ApporintmentProvider',
+        columnNames: ['provider_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('appointments', 'ApporintmentProvider');
+  }
+}
