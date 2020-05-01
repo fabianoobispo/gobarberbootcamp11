290d5b82d84bb035c4fbe0c09eacb511cdb106b1
fbc623 <fbc623gmail.com>
2020-05-01 01:43:35 -0300

    cirado app

---
+++ app/src/components/Button/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { RectButtonProperties } from 'react-native-gesture-handler';
+import { Container, Buttontext } from './styles';
+
+interface ButtonProps extends RectButtonProperties {
+  children: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
+  <Container {...rest}>
+    <Buttontext>{children}</Buttontext>
+  </Container>
+);
+
+export default Button;
