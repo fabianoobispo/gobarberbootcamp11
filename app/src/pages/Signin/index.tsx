44cc058e5cce1ae7fa6665838a2bbc52b904f6a2
fbc623 <fbc623gmail.com>
2020-05-03 15:31:29 -0300

    s

--- app/src/pages/Signin/index.tsx
+++ app/src/pages/Signin/index.tsx
@@ -1,30 +1,66 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+  Image,
+  View,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+} from 'react-native';
 
+import Icon from 'react-native-vector-icons/Feather';
 import logoImg from '../../assets/logo.png';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgodPasswordText,
+  CreateAccountbutton,
+  CreateAccountbuttonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-
-      <Title>Faça seu logon</Title>
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
-      <Button
-        onPress={() => {
-          console.log('foi ');
-        }}
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
       >
-        {' '}
-        Entrar
-      </Button>
-    </Container>
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+            <Button
+              onPress={() => {
+                console.log('foi ');
+              }}
+            >
+              Entrar
+            </Button>
+
+            <ForgotPassword onPress={() => {}}>
+              <ForgodPasswordText>Esqueci minha senha</ForgodPasswordText>
+            </ForgotPassword>
+          </Container>
+
+          <CreateAccountbutton onPress={() => {}}>
+            <Icon name="log-in" size={20} color="#ff9000" />
+            <CreateAccountbuttonText>Criar uma conta</CreateAccountbuttonText>
+          </CreateAccountbutton>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </>
   );
 };
 export default SignIn;
